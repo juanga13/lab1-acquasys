ca6b9cce85bee3462fb8f878591af3bcda12c14d
Juanga Ricci <riccijuanga@gmail.com>
2019-05-22 10:04:04 -0300

    new lesson

--- src/app/components/Account/owner/NewLesson.jsx
+++ src/app/components/Account/owner/NewLesson.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Form, Row} from 'react-bootstrap';
+import {Button, Form, Row, Dropdown, Col} from 'react-bootstrap';
 import ReactModal from 'react-modal';
 
 class NewClass extends Component {
@@ -17,10 +17,10 @@ class NewClass extends Component {
         minutes: false,
       },
       name: "",
-      duration: -1,
-      weekday: -1,
-      hour: -1,
-      minutes: -1,
+      duration: '',
+      weekday: "Seleccione un dia",
+      hour: '',
+      minutes: '',
       date: '',
     }
   }
@@ -35,17 +35,22 @@ class NewClass extends Component {
       isModalOpen: false,
       name: "",
       // default number value (invalid) is -1
-      duration: -1,
-      weekday: -1,
-      hour: -1,
-      minutes: -1
+      duration: "",
+      weekday: "",
+      hour: "",
+      minutes: ""
     })
   };
 
   handleChange = event => {
-    console.log(this.state.date);
+    console.log(event.target.value);
+    console.log(event.target.id === "minutes" && parseInt(event.target.value) > 59);
     event.preventDefault();
-    this.setState({ [event.target.id]: event.target.value });
+    if (event.target.id === "hour" && parseInt(event.target.value) > 23) 
+      this.setState({hour: 23});
+    else if (event.target.id === "minutes" && parseInt(event.target.value) >59)
+      this.setState({minutes: 59});
+    else this.setState({ [event.target.id]: event.target.value });
   };
 
   validateInputs() {
@@ -57,17 +62,17 @@ class NewClass extends Component {
     const minutes = this.state.minutes;
     this.setState({ errors: {
       name: (name.length === 0),
-      duration: (duration.length === -1),  // minutes number input
-      weekday: (weekday === -1),  // dropdown of seven days
-      hour: (hour === -1),  // starting time, dropdown from 06:00 to 05:00
-      minutes: (minutes === -1)  // starting time, number input
+      duration: (duration === ''),  // minutes number input
+      weekday: (weekday === 'Seleccione un dia'),  // dropdown of seven days
+      hour: (hour === ''),  // starting time, dropdown from 06:00 to 05:00
+      minutes: (minutes === '')  // starting time, number input
     }})
     return ( 
       name.length === 0
-      || duration.length === -1
-      || weekday === -1
-      || hour === -1 
-      || minutes === -1
+      || duration.length === 0
+      || weekday === 'Seleccione un dia'
+      || hour === ''
+      || minutes === ''
     )
   }
 
@@ -87,7 +92,7 @@ class NewClass extends Component {
     };
 
     console.log("token: " + this.state.token);
-    fetch("http://172.22.44.128:8080/api/admin/createTeacher", {
+    fetch("http://172.22.44.128:8080/api/admin/createLesson", {
       method: "POST",
       mode: "cors",
       cache: "no-cache",
@@ -102,9 +107,12 @@ class NewClass extends Component {
     })
       .then(response => {
         if (response.ok) {
+          // data is valid and could add to db
           console.log(response);
           this.setState({ registerSuccess: true });
+          this.cancelModal();
         } else {
+          // data is valid but cant add to db
           console.log(response);
         }
       })
@@ -120,16 +128,21 @@ class NewClass extends Component {
     }
   };
 
-  // name, duration, weekday, hour, minutes
-  
-  // renderNameError = () => {
-  //   return (this.state.errors.name) && <h6 className="text-danger">Nombre invalido</h6> };
-  // renderDurationError = () => {
-  //   return (this.state.errors.email) && <h6 className="text-danger">Duracion invalida</h6> };
-  // renderPasswordError = () => {
-  //   return (this.state.errors.password) && <h6 className="text-danger">Contrasena invalida</h6> };
-  // renderSurnameError = () => {
-  //   return (this.state.errors.surname) && <h6 className="text-danger">Apellido invalido</h6> };
+  renderNameError = () => {
+    return (this.state.errors.name) && <h6 className="text-danger">Nombre invalido</h6> };
+  renderWeekdayError = () => {
+    console.log(this.state.weekday);
+    return (this.state.errors.weekday) && <h6 className="text-danger">Dia invalido</h6> };
+  renderDurationError = () => {
+    return (this.state.errors.duration) && <h6 className="text-danger">Duracion invalida</h6> };
+  renderHourMinutesError = () => {
+      return (this.state.errors.hour || this.state.errors.minutes) && <h6 className="text-danger">Horario invalido</h6> };
+
+  handleWeekDayChange = event => {
+    // event.preventDefault();
+    console.log(event);
+    this.setState({weekday: event});
+  }
 
   render() {
     return (
@@ -137,7 +150,12 @@ class NewClass extends Component {
         <h2>Alumnos</h2>
         {this.renderNotification()}
         <Button onClick={this.handleAddStudent}>Agregar un nuevo alumno</Button>
-        
+        {/* name: "",
+      duration: -1,
+      weekday: -1,
+      hour: -1,
+      minutes: -1,
+      date: '', */}
         <ReactModal
           className="modal-form"
           isOpen={this.state.isModalOpen}
@@ -146,50 +164,68 @@ class NewClass extends Component {
         >
           <Form onSubmit={this.handleSubmit} >
             <Form.Group>
-              <Form.Label>Correo Electronico</Form.Label>
-              <Form.Control
-                id="email"
-                type="email"
-                autoFocus
-                placeholder="Email"
-                className={(this.state.errors.email) && "border border-danger"}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            {/* {this.renderEmailError()} */}
-            <Form.Group>
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control
-                id="password"
-                type="password"
-                placeholder="Contraseña"
-                className={(this.state.errors.password) && "border border-danger"}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            {/* {this.renderPasswordError()} */}
-            <Form.Group>
               <Form.Label>Nombre</Form.Label>
               <Form.Control
                 id="name"
                 type="name"
+                autoFocus
                 placeholder="Nombre"
                 className={(this.state.errors.name) && "border border-danger"}
                 onChange={this.handleChange}
               />
             </Form.Group>
-            {/* {this.renderNameError()} */}
+            {this.renderNameError()}
             <Form.Group>
-              <Form.Label>Apellido</Form.Label>
+              <Dropdown 
+                title={this.state.weekday} 
+                className={(this.state.errors.weekday) && "border border-danger"}
+              >
+                {["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"].map(
+                  (weekday) => 
+                    <Dropdown.Item 
+                      className="bg-default border border-dark" 
+                      onClick={() => this.handleWeekDayChange(weekday)}
+                    >
+                      {weekday}
+                  </Dropdown.Item>
+                )}
+                </Dropdown>
+            </Form.Group>
+            {this.renderWeekdayError()}
+            <Form.Group>
+              <Form.Label>Duracion (en minutos)</Form.Label>
               <Form.Control
-                id="surname"
-                type="name"
-                placeholder="Apellido"
-                className={(this.state.errors.surname) && "border border-danger"}
+                id="duration"
+                type="number"
+                placeholder="minutos"
+                className={(this.state.errors.duration) && "border border-danger"}
                 onChange={this.handleChange}
               />
             </Form.Group>
-            {/* {this.renderSurnameError()} */}
+            {this.renderDurationError()}
+            {/* const hourRange = [0, 23];}*/}
+            {/*const minutesRange = [0, 59]; */}
+            <Form.Group as={Row}>
+              <Form.Label column>Hora de inicio</Form.Label>
+              <Col><Form.Control
+                id="hour"
+                type="number"
+                placeholder="16"
+                value={this.state.hour}
+                className={(this.state.errors.hour) && "border border-danger"}
+                onChange={this.handleChange}
+              /></Col>
+              <span className="m-auto">:</span>
+              <Col><Form.Control
+                id="minutes"
+                type="number"
+                placeholder="58"
+                value={this.state.minutes}
+                className={(this.state.errors.minutes) && "border border-danger"}
+                onChange={this.handleChange}
+              /></Col>
+            </Form.Group>
+            {this.renderHourMinutesError()}
             <Row className="modal-form-button-container">
               <Button type="submit">Agregar nuevo profesor</Button>
               <Button
