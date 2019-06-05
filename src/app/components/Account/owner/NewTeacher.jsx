4822a9b682fabf707f677aae4d89e9551ff8c61e
Juanga Ricci <riccijuanga@gmail.com>
2019-06-05 12:03:30 -0300

    new teacher edit/delete

--- src/app/components/Account/owner/NewTeacher.jsx
+++ src/app/components/Account/owner/NewTeacher.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
 import { Button, Form, Row } from 'react-bootstrap';
 import ReactModal from 'react-modal';
 import '../../../css/modal-form.css'
@@ -10,55 +9,59 @@ class NewTeacher extends Component {
     super(props);
 
     this.state = {
-      lessons: null,
-      loadedStudents: false,
+      token: localStorage.getItem("token"),
+      
+      teachers: null,
+      loadedTeachers: false,
       registerSuccess: false,
       editSuccess: false,
       deleteSuccess: false,
-      token: localStorage.getItem("token"),
+      
       isModalOpen: false,
       showEditModal: false,
-      editDNI: '',
+      editCUIL: '',
       showDeleteModal: false,
-      deleteDNI: '',      
+      deleteCUIL: '',
+
       errors: {
         email: false,
         password: false,
         name: false,
-        surname: false
+        surname: false,
+        cuil: false,
       },
       id: null,
-      email: "", 
-      password: "", 
-      name: "",  
-      surname: "" 
+      email: "",
+      password: "",
+      name: "",
+      surname: "",
+      cuil: '',
     }
   }
 
   componentDidMount() {
     fetch(RequestManager.baseUrl + "/api/teacher/all", {
-        method: "GET",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers:
-            {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + this.state.token
-            },
-        redirect: "follow",
-        referrer: "no-referrer",
+      method: "GET",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + this.state.token
+      },
+      redirect: "follow",
+      referrer: "no-referrer",
     })
-        .then(response => {
-            return response.json()
-        }).then(teachers => {
-        this.setState({teachers: teachers, loadedTeachers: true});
-    })
-}
-
+      .then(response => {
+        return response.json()
+      }).then(teachers => {
+        this.setState({ teachers: teachers, loadedTeachers: true });
+      })
+  };
 
   handleAddTeacher = event => {
     event.preventDefault();
+    this.resetSuccessBools();
     this.setState({ isModalOpen: true });
   };
 
@@ -68,7 +71,8 @@ class NewTeacher extends Component {
       email: '',
       password: '',
       name: '',
-      surname: ''
+      surname: '',
+      cuil: '',
     })
   };
 
@@ -78,26 +82,31 @@ class NewTeacher extends Component {
   };
 
   validateInputs() {
-    console.log("current state: " + this.state.email);
+    // console.log("current state: " + this.state.email);
     const email = this.state.email;
     const password = this.state.password;
     const name = this.state.name;
     const surname = this.state.surname;
-    this.setState({ errors: {
-      email: (email.length === 0 ||
-        !email.includes("@") ||
-        !email.includes(".")),
-      password: (password.length < 6),
-      name: (name.length === 0),
-      surname: (surname.length === 0)
-    }})
+    const cuil = this.state.cuil;
+    this.setState({
+      errors: {
+        email: (email.length === 0 ||
+          !email.includes("@") ||
+          !email.includes(".")),
+        password: (password.length < 6),
+        name: (name.length === 0),
+        surname: (surname.length === 0),
+        cuil: (cuil.length === 0)
+      }
+    })
   }
 
   handleSubmit = event => {
     event.preventDefault();
     this.validateInputs();
     if (this.state.errors.email || this.state.errors.password ||
-      this.state.errors.name || this.state.errors.surname) {
+      this.state.errors.name || this.state.errors.surname 
+      || this.state.errors.cuil) {
       event.stopPropagation();
       return;
     }
@@ -106,19 +115,47 @@ class NewTeacher extends Component {
       email: this.state.email,
       password: this.state.password,
       name: this.state.name,
-      surname: this.state.surname
+      surname: this.state.surname,
+      cuil: this.state.cuil
     };
 
-    console.log("token: " + this.state.token);
-    fetch("http://172.22.44.128:8080/api/admin/createTeacher", {
+    // console.log("token: " + this.state.token);
+    (this.state.id)
+    ? (fetch(RequestManager.baseUrl + '/api/teacher/update', {
+      method: "PUT",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers:
+      {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + this.state.token
+      },
+      redirect: "follow",
+      referrer: "no-referrer",
+      body: JSON.stringify(data),
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log(response);
+          this.setState({ editSuccess: true });
+        } else {
+          console.log(response);
+        }
+      })
+      .catch(error => {
+        console.log("Error: " + error)
+    }))
+    : (fetch(RequestManager.baseUrl + "/api/admin/createTeacher", {
       method: "POST",
       mode: "cors",
       cache: "no-cache",
       credentials: "same-origin",
-      headers: 
-        { "Content-Type": "application/json",
-          "Authorization": "Bearer " + this.state.token
-        },
+      headers:
+      {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + this.state.token
+      },
       redirect: "follow",
       referrer: "no-referrer",
       body: JSON.stringify(data),
@@ -133,14 +170,95 @@ class NewTeacher extends Component {
       })
       .catch(error => {
         console.log("Error: " + error)
-      })
+    }));
+  };
+
+  resetSuccessBools = () => {
+    this.setState({registerSuccess: false, editSuccess: false, deleteSuccess: false});
+  };
+
+  handleEditTeacher = teacher => {
+    // event.preventDefault();
+    this.resetSuccessBools();
+    this.setState({
+      id: teacher.id,
+      email: teacher.email,
+      password: '',
+      name: teacher.name,
+      surname: teacher.surname,
+      cuil: teacher.cuil,
+
+      editTeacher: teacher,
+      isModalOpen: true,
+    });
+  };
+
+  handleDeleteTeacher = teacher => {
+    // event.preventDefault();
+    this.resetSuccessBools();
+    fetch(RequestManager.baseUrl + "/api/teacher/delete/" + teacher.id, {
+        method: "DELETE",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers:
+        {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + this.state.token
+        },
+        redirect: "follow",
+        referrer: "no-referrer",
+    })
+        .then(response => {
+            if (response.status === 200) {
+                this.setState({ deleteSuccess: true })
+            }
+        }).then(x => {
+            this.componentDidMount(); // Geteame la lista denuevo jajajajajaja
+            // TODO: no seas negro facundo sacalo a manopla de la lista en vez de traer todo de nuevo JAJAJAJAJA
+            //PD: BiCONeee!
+        })
   };
 
   renderNotification = () => {
     if (this.state.registerSuccess) {
-      this.setState({ registerSuccess: false });
       return <h6 className="text-success">Nuevo profesor registrado correctamente</h6>;
     }
+    if (this.state.editSuccess) {
+      return <h6 className="text-success">Profesor editado correctamente</h6>;
+    }
+    if (this.state.deleteSuccess) {
+      return <h6 className="text-success">Profesor eliminado correctamente</h6>;
+    }
+  };
+
+  renderTeachers = () => {
+    const teachers = this.state.teachers;
+    // console.log(this.state.loadedTeachers + " xd " + teachers);
+    if (this.state.loadedTeachers) {
+      return (
+        <div>
+          <Row>
+            <h6 className='p-2 m-auto'>Nombre</h6>
+            <h6 className='p-2 m-auto'>Apellido</h6>
+            <h6 className='p-2 m-auto'>CUIL</h6>
+          </Row>
+          {(teachers) ? teachers.map((teacher) => (
+            <Row id={teacher.id}>
+              <h6 className='p-2 m-auto'>{teacher.name}</h6>
+              <h6 className='p-2 m-auto'>{teacher.surname}</h6>
+              <h6 className='p-2 m-auto'>{teacher.cuil}</h6>
+              <Button 
+                className='btn btn-secondary p-2 m-auto' 
+                onClick={() => this.handleEditTeacher(teacher)}>Editar</Button>
+              <Button
+                className='btn btn-danger p-2 m-auto' 
+                onClick={() => this.handleDeleteTeacher(teacher)}>Eliminar</Button>
+            </Row>))
+            : <h5>Lista vacia!</h5>}
+        </div>
+      );
+    } else return <h5>Cargando...</h5>;
   };
 
   render() {
@@ -148,11 +266,12 @@ class NewTeacher extends Component {
       <div>
         <h2>Profesores</h2>
         {this.renderNotification()}
-        <Button onClick={this.handleAddTeacher}>Agregar nueva clase</Button>
-        <ReactModal 
+        <Button onClick={this.handleAddTeacher}>Agregar nuevo profesor</Button>
+        {this.renderTeachers()}
+
+        <ReactModal
           className="modal-form"
           isOpen={this.state.isModalOpen}
-          contentLabel="Add teacher modal"
         >
           <Form onSubmit={this.handleSubmit} >
             <Form.Group>
@@ -200,6 +319,17 @@ class NewTeacher extends Component {
               />
             </Form.Group>
             {(this.state.errors.surname) && <h6 className="text-danger">Apellido invalido</h6>}
+            <Form.Group>
+              <Form.Label>CUIL</Form.Label>
+              <Form.Control
+                id="cuil"
+                type="name"
+                placeholder="CUIL"
+                className={(this.state.errors.cuil) && "border border-danger"}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            {(this.state.errors.cuil) && <h6 className="text-danger">Apellido invalido</h6>}
             <Row className="modal-form-button-container">
               <Button type="submit">Agregar nuevo profesor</Button>
               <Button
@@ -213,9 +343,5 @@ class NewTeacher extends Component {
     );
   }
 }
-
-const mapStateToProps = state => {return ({token: state.token})};
-
-NewTeacher = connect(mapStateToProps)(NewTeacher);
 
 export default NewTeacher;
